4f6417a6068751fc8b03ca5dd37c3302ea88f57d
Louis Lugas <louislugas@gmail.com>
2025-12-26 23:39:01 +0700

    update broadcast & presence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,70 @@
 <script>
+	let user = Math.floor(Math.random()*9999)
+
+
 	import {supabase} from '$lib/supabase';
 
-	const myChannel = supabase.channel('test-channel')
+	let mY = 0
+	let progress = 0
+	let path
+	let xPos
+	let yPos
+
+	const myChannel = supabase.channel('test-channel',{
+		config: {
+			broadcast: {
+				self: true // Receive your own broadcasts
+			}
+		}
+	});
+
+	// Simple function to log any messages we receive
+	function messageReceived(payload) {
+		console.log(payload, "message received")
+	}
+
+	myChannel
+		.on('presence', { event: 'join' }, ({ key, newPresences }) => {
+			console.log('join', key, newPresences)
+		})
+
+	// Subscribe to the Channel
+	myChannel
+	.on(
+		'broadcast',
+		{ event: 'shout' }, // Listen for "shout". Can be "*" to listen to all events
+		(payload) => messageReceived(payload)
+	)
+	.subscribe()
+
+	// Send a test message
+	myChannel
+		.send({
+			type: 'broadcast',
+			event: 'shout',
+			payload: { message: 'Hi' , userId: user},
+		})
+		.then((resp) => console.log(resp))
+
+	function mousemove(e) {
+		// if (progress <= path.getTotalLength()) {
+			mY = e.movementY
+			progress += Math.abs(mY)/100
+			// console.log(parseInt(progress))
+			// xPos = path.getPointAtLength(progress).x
+			// yPos = path.getPointAtLength(progress).y
+		// }
+	}
+
 </script>
+
+<svelte:body on:mousemove={mousemove}></svelte:body>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding:0;
+		width:100%;
+		height: 100vh;
+	}
+</style>
