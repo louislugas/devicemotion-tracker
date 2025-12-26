61322d38a1339d400243e39998a45f2f1119f152
Louis Lugas <louislugas@gmail.com>
2025-12-26 23:45:34 +0700

    update realtime presence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,9 +24,17 @@
 	}
 
 	myChannel
+		.on('presence', { event: 'sync' }, () => {
+			const newState = roomOne.presenceState()
+			console.log('sync', newState)
+		})
 		.on('presence', { event: 'join' }, ({ key, newPresences }) => {
 			console.log('join', key, newPresences)
 		})
+		.on('presence', { event: 'leave' }, ({ key, leftPresences }) => {
+			console.log('leave', key, leftPresences)
+		})
+		.subscribe()
 
 	// Subscribe to the Channel
 	myChannel
