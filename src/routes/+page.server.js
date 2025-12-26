57a8d852a6e9f60b5ebe44dedcb7b43d8ee66a86
Louis Lugas <louislugas@gmail.com>
2025-12-26 23:25:32 +0700

    first commit

---
+++ src/routes/+page.server.js
@@ -0,0 +1,10 @@
+/** @type {import('./$types').PageServerLoad} */
+
+import { supabase } from "$lib/supabase";
+  export async function load() {
+
+    // const { data } = await supabase.from("instruments").select();
+    // return {
+    //   instruments: data ?? [],
+    // };
+  }
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import {supabase} from '$lib/supabase';
+
+	const myChannel = supabase.channel('test-channel')
+</script>
